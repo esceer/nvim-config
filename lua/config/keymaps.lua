07e43b98ce379088fd3c37e5e2a046b90e96780b
Roland Kutas <roland.kutas@dmgmori-heitec.hu>
2025-04-26 20:40:03 +0200

    Redo neovim config from scratch + add some basic keymaps and options

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,38 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local keymap = vim.keymap
+local opts = { noremap = true, silent = true }
+
+-- Select all
+keymap.set("n", "<C-a>", "gg<S-v>G")
+
+-- Move window
+keymap.set("n", "<C-left>", "<C-w>h", opts)
+keymap.set("n", "<C-right>", "<C-w>l", opts)
+keymap.set("n", "<C-up>", "<C-w>k", opts)
+keymap.set("n", "<C-down>", "<C-w>j", opts)
+
+-- Resize window
+keymap.set("n", "<C-h>", "<C-w><", opts)
+keymap.set("n", "<C-l>", "<C-w>>", opts)
+keymap.set("n", "<C-k>", "<C-w>+", opts)
+keymap.set("n", "<C-j>", "<C-w>-", opts)
+
+-- Split window
+keymap.set("n", "sh", ":split<Return>", opts)
+keymap.set("n", "sv", ":vsplit<Return>", opts)
+
+-- Diagnostics
+-- keymap.set("n", "<C-J>", function()
+--   vim.diagnostic.goto({ next = true })
+-- end, opts)
+
+-- Open function info popup
+keymap.set("n", "<C-i>", vim.lsp.buf.signature_help, {
+  desc = "Show signature help",
+})
+
+-- Terminal
+vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]], opts)
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,18 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Turn of paste mode when leaving insert
+vim.api.nvim_create_autocmd("InsertLeave", {
+  pattern = "*",
+  command = "set nopaste",
+})
+
+-- Fix conceallevel for json files
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "json", "jsonc" },
+  callback = function()
+    vim.wo.spell = false
+    vim.wo.conceallevel = 0
+  end,
+})
