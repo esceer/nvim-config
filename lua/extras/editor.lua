3eeafa4abdb269b31508e99a44820ee1906bbb89
Roland Kutas <roland.kutas@dmgmori-heitec.hu>
2025-04-28 01:32:07 +0200

    Restructure extras config

---
+++ lua/extras/editor.lua
@@ -0,0 +1,8 @@
+return {
+  { import = "lazyvim.plugins.extras.coding.nvim-cmp" },
+  { import = "lazyvim.plugins.extras.editor.outline" },
+  { import = "lazyvim.plugins.extras.editor.telescope" },
+  { import = "lazyvim.plugins.extras.formatting.prettier" },
+  { import = "lazyvim.plugins.extras.linting.eslint" },
+  { import = "lazyvim.plugins.extras.ui.treesitter-context" },
+}
---
+++ lua/extras/langs.lua
@@ -0,0 +1,11 @@
+return {
+  { import = "lazyvim.plugins.extras.lang.angular" },
+  { import = "lazyvim.plugins.extras.lang.docker" },
+  { import = "lazyvim.plugins.extras.lang.go" },
+  { import = "lazyvim.plugins.extras.lang.json" },
+  { import = "lazyvim.plugins.extras.lang.markdown" },
+  { import = "lazyvim.plugins.extras.lang.typescript" },
+  { import = "lazyvim.plugins.extras.lang.yaml" },
+
+  { import = "lazyvim.plugins.extras.lsp.none-ls" },
+}
