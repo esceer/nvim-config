f849f17a088aaae01718d6d0a0d8df541188e65c
Roland Kutas <roland.kutas@dmgmori-heitec.hu>
2025-04-27 16:22:36 +0200

    Set default color scheme

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "tokyonight",
+    },
+  },
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      style = "storm", -- Set the storm variant
+    },
+  },
+}
